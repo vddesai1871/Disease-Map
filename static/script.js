2f421effc8b038c200ef09c5a38fd272b513fd2d
Vishal Desai <vddesai1998@gmail.com>
2018-01-20 19:13:38 +0530

    Updated JS file to work with API key from backend

--- static/script.js
+++ static/script.js
@@ -200,7 +200,7 @@ function update(place)
     for(var i = 0; i <place.length; i++) {
         var parameters = {
             address: place[i] + ",Gujarat",
-            key: "Your Google Map API key goes here." 
+            key: key
         };
 
 
